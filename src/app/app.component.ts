a65c72e9742401d34affa14571a8c9a6b2777188
Oğuzhan ÇART <54273323+oguzhan18@users.noreply.github.com>
2023-07-17 14:57:52 +0300

    test fonksiyon added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+ 
   title = 'as-ui-app';
+  test(){
+    console.log(this.title);
+    
+  }
+ 
 }
