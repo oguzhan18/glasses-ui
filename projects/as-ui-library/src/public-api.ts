8705cc4f8407598e418a4a968031164a68b16e2e
oguzhan18 <oguzhancart1@gmail.com>
2023-07-22 02:17:50 +0300

    added feature possetion  varebile

--- projects/as-ui-library/src/public-api.ts
+++ projects/as-ui-library/src/public-api.ts
@@ -1,7 +1,1 @@
-/*
- * Public API Surface of as-ui-library
- */
 
-export * from './lib/as-ui-library.service';
-export * from './lib/as-ui-library.component';
-export * from './lib/as-ui-library.module';
